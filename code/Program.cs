1d18b92226fb922d65dde22399f5ef987cdb3352
RazeVenStudios <razevenstudios@gmail.com>
2023-10-06 17:31:28 -0400

    Binary to Decimal converter

--- code/Program.cs
+++ code/Program.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿            myMethod();
+            question();
+            static void myMethod() {
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("Input the binary value you want converted!");
+                Console.WriteLine("------------------------------------------");
 
-namespace Program // Note: actual namespace depends on the project name.
-{
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+                int binaryVal = Convert.ToInt32((Console.ReadLine()));
+                int decimalVal = Convert.ToInt32(binaryVal.ToString(), 2);
+                Console.WriteLine($"Your binary value is equal to {decimalVal} as a decimal!");
+            }
+            static void question() {
+                Console.WriteLine("Do you want to convert again? Y/N");
+                string response = Console.ReadLine().ToUpper();
+                bool loopYesNo = true;
+                while (loopYesNo == true) {
+                    if (response == "Y"){
+                        myMethod();
+                        question();
+                    }
+                    else if (response == "N"){
+                        Console.WriteLine("Have a nice day");
+                        Environment.Exit(0);
+                    }
+                    else{
+                         Console.WriteLine("Please type a valid response");
+                         question();
+                    }
+                }
+            }
+
+
