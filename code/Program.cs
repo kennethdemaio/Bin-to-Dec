9533767410243eced7dd0d9af1f3f4afd4e8039c
RazeVenStudios <razevenstudios@gmail.com>
2023-10-07 15:48:52 -0400

    Complete refactor

--- code/Program.cs
+++ code/Program.cs
@@ -1,35 +1,38 @@
-﻿Console.WriteLine("------------------------------------------");
-Console.WriteLine("Input the binary value you want converted!");
-Console.WriteLine("------------------------------------------");
-string UserInput = Console.ReadLine();
-if (UserInput != null && UserInput.Length < 9){
-    ConversionMethod(UserInput);
+﻿bool RunProgram = true;
+while (RunProgram){
+    Console.WriteLine("------------------------------------------");
+    Console.WriteLine("Input the binary value you want converted!");
+    Console.WriteLine("------------------------------------------");
+    string UserInput = Console.ReadLine();
+    if (int.TryParse(UserInput, out int number) && UserInput != null && UserInput.Length < 9){
+        ConversionMethod(UserInput);
     }
-else{
-    Console.WriteLine("Binary has a max value of 8 digits.");
-}
-static void ConversionMethod(string UserInput){
-    int DecimalValue = Convert.ToInt32(UserInput);
-    DecimalValue = Convert.ToInt32(DecimalValue.ToString(), 2);
-    Console.WriteLine($"Your binary value is equal to {DecimalValue} as a decimal!");
-}
-/*static void QuestionMethod(){
-    Console.WriteLine("Do you want to convert again? Y/N");
-    string response = Console.ReadLine().ToUpper();
-    bool loopYesNo = true;
-    while (loopYesNo == true){
-        if (response == "Y"){
-            ConversionMethod();
-            QuestionMethod();
+    else{
+        Console.WriteLine("Invalud input");
+    }
+    static void ConversionMethod(string UserInput){
+        int DecimalValue = Convert.ToInt32(UserInput);
+        DecimalValue = Convert.ToInt32(DecimalValue.ToString(), 2);
+        Console.WriteLine($"Your binary value is equal to {DecimalValue} as a decimal!");
+    }
+    bool Question = true;
+    while (Question)
+    {
+        Console.WriteLine("Do you want to convert again? Y/N");
+        string response = Console.ReadLine().ToUpper();
+        if (response == "Y") {
+            RunProgram = true;
+            Question = false;
         }
-        else if (response == "N"){
-            Console.WriteLine("Have a nice day");
+        else if (response == "N") {
+            RunProgram = false;
+            Question = false;
+            Console.WriteLine("Have a wonderful day!");
             Environment.Exit(0);
         }
-        else{
-             Console.WriteLine("Please type a valid response");
-             QuestionMethod();
+        else {
+            Console.WriteLine("Please input a valid response");
+            Question = true;
         }
     }
 }
-*/
