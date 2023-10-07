0f2b06bf8867d12d927d38289e8e87ef13e55aa4
RazeVenStudios <razevenstudios@gmail.com>
2023-10-07 14:42:39 -0400

    Reformatting

--- code/Program.cs
+++ code/Program.cs
@@ -1,30 +1,30 @@
-﻿myMethod();
-question();
-static void myMethod() {
+﻿ConversionMethod();
+QuestionMethod();
+static void ConversionMethod(){
     Console.WriteLine("------------------------------------------");
     Console.WriteLine("Input the binary value you want converted!");
     Console.WriteLine("------------------------------------------");
 
-    int binaryVal = Convert.ToInt32((Console.ReadLine()));
-    int decimalVal = Convert.ToInt32(binaryVal.ToString(), 2);
-    Console.WriteLine($"Your binary value is equal to {decimalVal} as a decimal!");
+    int binaryValue = Convert.ToInt32((Console.ReadLine()));
+    int decimalValue = Convert.ToInt32(binaryValue.ToString(), 2);
+    Console.WriteLine($"Your binary value is equal to {decimalValue} as a decimal!");
 }
-static void question() {
+static void QuestionMethod(){
     Console.WriteLine("Do you want to convert again? Y/N");
     string response = Console.ReadLine().ToUpper();
     bool loopYesNo = true;
     while (loopYesNo == true) {
         if (response == "Y"){
-            myMethod();
-            question();
+            ConversionMethod();
+            QuestionMethod();
         }
         else if (response == "N"){
             Console.WriteLine("Have a nice day");
             Environment.Exit(0);
         }
         else{
-                Console.WriteLine("Please type a valid response");
-                question();
+             Console.WriteLine("Please type a valid response");
+             QuestionMethod();
         }
     }
 }
